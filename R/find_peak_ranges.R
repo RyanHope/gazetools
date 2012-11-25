28f227cf65fa9d47cf3c104c01e8582fa9282f23
Ryan Hope <rmh3093@gmail.com>
2012-11-25 09:20:18 -0500

    Handle no peak conditions better

--- R/find_peak_ranges.R
+++ R/find_peak_ranges.R
@@ -1,7 +1,9 @@
 find_peak_ranges <- function(x, threshold)
 {
   t <- which(x>=threshold)
-  d <- diff(t)
-  n <- which(d>mean(d))  
-  data.frame(begin=c(t[1], t[n+1]), end=c(t[n], tail(t,1)))
+  if (length(t)>0) {
+    d <- diff(t)
+    n <- which(d>mean(d))  
+    data.frame(begin=c(t[1], t[n+1]), end=c(t[n], tail(t,1)))
+  } 
 }
--- R/find_peaks.R
+++ R/find_peaks.R
@@ -2,10 +2,12 @@ find_peaks <- function(x, threshold)
 {
   ranges <- find_peak_ranges(x, threshold)
   peaks <- NULL
-  for (i in 1:nrow(ranges)) {
-    rnge <- ranges[i,1]:ranges[i,2]
-    r <- x[rnge]
-    peaks <- c(peaks,rnge[which(r==max(r))])
+  if (!is.null(ranges)) {
+    for (i in 1:nrow(ranges)) {
+      rnge <- ranges[i,1]:ranges[i,2]
+      r <- x[rnge]
+      peaks <- c(peaks,rnge[which(r==max(r))])
+    }
+    peaks
   }
-  peaks
 }
