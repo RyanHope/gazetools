804808c8d2e9a8317cb9aa1b03f69fd7c16d5b6d
Ryan M. Hope <rmh3093@gmail.com>
2013-08-30 11:07:42 -0400

    fix nndist usage

--- R/nni.R
+++ R/nni.R
@@ -9,7 +9,7 @@
 #' is random. Values lower than 1 suggest grouping, whereas values higher than 1 suggest 
 #' regularity (i.e. the point pattern is dispersed in a non-random way). 
 #'
-#' @importFrom spatstat nndist
+#' @importFrom spatstat nndist.default
 #' @export
 #' 
 #' @example example/pva.R
@@ -19,5 +19,5 @@
 #' @example example/nni-out.R
 #' 
 nni <- function(x) {
-  mean(nndist(x))/(.5*sqrt(chull_area(x)/nrow(x)))
+  mean(nndist.default(x))/(.5*sqrt(chull_area(x)/nrow(x)))
 }
