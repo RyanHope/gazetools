9f8874768c9c172500490a6597d99c39c3b0b7c6
Ryan Hope <rmh3093@gmail.com>
2013-11-14 15:46:05 -0500

    CHECK THIS!!!!!

--- OLD/aaa-classes.R
+++ OLD/aaa-classes.R
@@ -1,32 +1,6 @@
 
 
-#' Class "mould"
-#'
-#' A class to hold mould threshold information
-#'
-#'@section Slots: 
-#'  \describe{
-#'    \item{\code{.Data}}{a threshold value}
-#'    \item{\code{thresholds}}{vector of class \code{"numeric"}}
-#'    \item{\code{resp1}}{vector of class \code{"numeric"}}
-#'    \item{\code{resp2}}{vector of class \code{"numeric"}}
-#'    \item{\code{gap}}{vector of class \code{"numeric"}}
-#'  }
-#'
-#' @importFrom methods setClass
-#' 
-#' @docType class
-#' @name mould-class
-#' @rdname mould-class
-#' 
-#' @export
-#' 
-setClass("mould", 
-         representation(thresholds = "numeric",
-                        resp1 = "numeric",
-                        resp2 = "numeric",
-                        gap = "numeric"), 
-         contains = "numeric")
+
 
 #' Class "ROI"
 #'
--- R/mouldThreshold.R
+++ R/mouldThreshold.R
@@ -1,3 +1,31 @@
+#' Class "mould"
+#'
+#' A class to hold mould threshold information
+#'
+#'@section Slots: 
+#'  \describe{
+#'    \item{\code{.Data}}{a threshold value}
+#'    \item{\code{thresholds}}{vector of class \code{"numeric"}}
+#'    \item{\code{resp1}}{vector of class \code{"numeric"}}
+#'    \item{\code{resp2}}{vector of class \code{"numeric"}}
+#'    \item{\code{gap}}{vector of class \code{"numeric"}}
+#'  }
+#'
+#' @importFrom methods setClass
+#' 
+#' @docType class
+#' @name mould-class
+#' @rdname mould-class
+#' 
+#' @export
+#' 
+setClass("mould", 
+         representation(thresholds = "numeric",
+                        resp1 = "numeric",
+                        resp2 = "numeric",
+                        gap = "numeric"), 
+         contains = "numeric")
+
 #' Mould Threshold
 #' 
 #' Uses the Mould algorithm to determine the optimal velocity or acceleration threshold for
