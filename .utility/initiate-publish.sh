e25023e3efb711030bd678e8d1873faad500adc8
Ryan Hope <rmh3093@gmail.com>
2014-03-03 21:32:58 -0500

    asdas

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -3,12 +3,10 @@ oi#!/bin/bash
 echo -e "Publishing staticdocs...\n"
 export
 
-ls -lah
+ls -lah inst
 
 cp -r inst/web ${HOME}
 cd $HOME
-
-ls -lah
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
@@ -20,6 +18,6 @@ git rm -rf *
 cp -Rf $HOME/web/* .
 git add -A
 git commit -m "Lastest staticdoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-git push -fq origin gh-pages# > /dev/null
+git push -fq origin gh-pages
 
 echo -e "Published staticdocs to gh-pages.\n"
