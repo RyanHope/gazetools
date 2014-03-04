e3b3df4cda3cc79b066f28192e8351649d7b9cc1
travis-ci <travis@travis-ci.org>
2014-03-03 20:34:19 -0500

    kkjahsdkjah

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -2,10 +2,13 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "ryanhope/gazetools" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
+  echo -e "Publishing staticdocs...\n"
+
   cd $HOME
+  ls
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ryanhope/gazetools gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ryanhope/gazetools gh-pages# > /dev/null
   
   # Commit and Push the Changes
   cd gh-pages
@@ -13,6 +16,8 @@ if [ "$TRAVIS_REPO_SLUG" == "ryanhope/gazetools" ] && [ "$TRAVIS_PULL_REQUEST" =
   cp -Rf $HOME/inst/web/* .
   git add -A
   git commit -m "Lastest staticdoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages# > /dev/null
+
+  echo -e "Published staticdocs to gh-pages.\n"
 
 fi
