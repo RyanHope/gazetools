9023a9b9de1c5ce84ab9a595898911fcf0e3a7ee
travis-ci <travis@travis-ci.org>
2014-03-03 20:44:33 -0500

    Asda

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "ryanhope/gazetools" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+echo -e "Publishing staticdocs...\n"
 
-  echo -e "Publishing staticdocs...\n"
+ls
+cd $HOME
+ls
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ryanhope/gazetools gh-pages# > /dev/null
 
-  ls
-  cd $HOME
-  ls
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ryanhope/gazetools gh-pages# > /dev/null
-  
-  # Commit and Push the Changes
-  cd gh-pages
-  git rm -rf *
-  cp -Rf $HOME/inst/web/* .
-  git add -A
-  git commit -m "Lastest staticdoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages# > /dev/null
+# Commit and Push the Changes
+cd gh-pages
+git rm -rf *
+cp -Rf $HOME/inst/web/* .
+git add -A
+git commit -m "Lastest staticdoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages# > /dev/null
 
-  echo -e "Published staticdocs to gh-pages.\n"
-
-fi
+echo -e "Published staticdocs to gh-pages.\n"
