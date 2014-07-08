c8f8afee5e9a20c7bf264a241abd3cf61d763310
Ryan Hope <rmh3093@gmail.com>
2014-07-07 22:22:13 -0400

    add support for splitting plots

--- R/pp-pva-classify-plot.R
+++ R/pp-pva-classify-plot.R
@@ -5,6 +5,7 @@ utils::globalVariables(c("variable","value"))
 #' Plot an object of class 'pva'
 #' 
 #' @param x an object of class \code{\link[=pva-class]{pva}}
+#' @param pages the number of pages to split the plot into
 #' 
 #' @export
 #' 
@@ -24,6 +25,7 @@ setMethod("plot", signature(x = "pva", y = "missing"), function(x, y, ...) pva.p
 #' 
 #' @param x an object of class \code{\link[=pva-class]{pva}}
 #' @param y an object of class \code{\link[=classify-class]{classify}} (optional)
+#' @param pages the number of pages to split the plot into
 #'
 #' @export
 #' 
@@ -49,10 +51,10 @@ setMethod("plot", signature(x = "pva", y = "classify"), function(x, y, ...) pva.
 #' @importFrom stats as.formula
 #' @importFrom reshape2 melt
 #' @importFrom ggplot2 ggplot aes_string geom_point geom_segment scale_color_manual scale_alpha_identity facet_grid theme xlab ylab coord_cartesian
-pva.plot <- function(x, y, ...)
+pva.plot <- function(x, y, pages=1, ...)
 {
+  p <- list()
   d <- as.data.frame(x)[,c("time","sx","sy","v","a")]
-  xlims <- c(min(d$time),max(d$time))
   if (!is.null(y) & class(y)=="classify") {
     d$class <- factor(y)
     d <- melt(d, id=c("time","class"))
@@ -77,13 +79,32 @@ pva.plot <- function(x, y, ...)
       if (!is.null(var))
         thresholds <- rbind(thresholds, data.frame(variable=var, intercept=y@thresholds[[threshold]]))
     }
-    thresholds$x <- xlims[1]
-    thresholds$xend <- xlims[2]
-    p <- ggplot(d) + geom_point(aes_string(x="time", y="value", color="class", alpha="quality")) + 
-      geom_segment(data=thresholds,aes_string(y="intercept",yend="intercept",x="x",xend="xend"), na.rm=T) +
-      scale_color_manual(values=c("black", "red", "green", "blue")) + scale_alpha_identity()
-  } else
-    p <- ggplot(d) + geom_point(aes_string(x="time", y="value"))
-  p + facet_grid(as.formula("variable~."), scales="free_y") + ylab("") + xlab("Time (s)") +
-    theme(legend.position = "top") + coord_cartesian(xlim=xlims)
+    if (pages > 1)
+      d <- split(d, cut(d$time, pages))
+    else
+      d <- list(d)
+    for (i in seq(length(d))) {
+      xlims <- c(min(d[[i]]$time),max(d[[i]]$time))
+      thresholds$x <- xlims[1]
+      thresholds$xend <- xlims[2]
+      p[[i]] <- ggplot(d[[i]]) + geom_point(aes_string(x="time", y="value", color="class", alpha="quality")) + 
+        geom_segment(data=thresholds,aes_string(y="intercept",yend="intercept",x="x",xend="xend"), na.rm=T) +
+        scale_color_manual(values=c("black", "red", "green", "blue")) + scale_alpha_identity() +
+        coord_cartesian(xlim=xlims)
+    }
+  } else {
+    if (pages > 1)
+      d <- split(d, cut(d$time, pages))
+    else
+      d <- list(d)
+    for (i in seq(length(d))) {
+      xlims <- c(min(d[[i]]$time),max(d[[i]]$time))
+      p[[i]] <- ggplot(d[[i]]) + geom_point(aes_string(x="time", y="value")) +
+        coord_cartesian(xlim=xlims)
+    }
+  }
+  for (i in seq(length(d)))
+    p[[i]] <- p[[i]] + facet_grid(as.formula("variable~."), scales="free_y") + ylab("") + xlab("Time (s)") +
+    theme(legend.position = "top")
+  p
 }
